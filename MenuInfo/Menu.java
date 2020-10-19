b5edf3a24a3cbfe72f9e94dcd663ea125ec8783c
Trung Duong Van <trunga1k16byt@gmail.com>
2020-10-19 07:52:11 +0700

    add MenuInfo folder

---
+++ MenuInfo/Menu.java
@@ -0,0 +1,32 @@
+package MenuInfo;
+
+public class Menu {
+
+	// thong tin mon an
+	private String name;
+	private double price;
+	
+	public Menu(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
