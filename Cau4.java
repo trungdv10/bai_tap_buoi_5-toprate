4c470f45afa094d115dbd89d359ea944eeb7245a
Trung Duong Van <trunga1k16byt@gmail.com>
2020-10-19 07:50:02 +0700

    add cau4

---
+++ Cau4.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Cau4 {
+
+	
+	/* Một lớp có thể tham chiếu bất kỳ kiểu đối tượng nào được gọi là lớp generic
+	 * 
+	 * 
+	 * 
+	 * */
+	
+	
+	public static void main(String args[]) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Java");
+        list.add("PHP");
+        list.add("C++");
+        // list.add(32);//compile time error
+ 
+        // show list
+        Iterator<String> itr = list.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
