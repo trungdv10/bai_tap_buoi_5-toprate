eb4a1f304571c0fdb1067c49683eb0c929dfb798
Trung Duong Van <trunga1k16byt@gmail.com>
2020-10-19 07:47:56 +0700

    add cau2

---
+++ Cau2.java
@@ -0,0 +1,14 @@
+
+public class Cau2 {
+	
+	/* StringBuilder và StringBuffer được tạo ra nhằm đáp ứng nhu cầu trong các trường hợp
+	 * muốn sử dụng đến chuỗi có khả năng thay đổi được.
+	 * StringBuilder và StringBuffer là khá giống nhau, điều khác biệt là các phương thức của StringBuffer đã được đồng bộ, 
+	 * thích hợp làm việc với ứng dụng đa luồng. Trong khi đó StringBuilder có các phương thức tương tự nhưng
+	 * không được đồng bộ, vì vậy mà hiệu suất cao hơn 
+	 * 
+	 * 
+	 * 
+	 * */
+	
+}
